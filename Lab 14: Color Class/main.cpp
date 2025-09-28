2704637e07ea1f65fe972eb98dbe52ddd18fdcb6
Guo An Wang <you@example.com>
2025-09-27 20:52:28 -0700

    second commit

--- Lab 14: Color Class/main.cpp
+++ Lab 14: Color Class/main.cpp
@@ -6,9 +6,23 @@
 //
 
 #include <iostream>
+using namespace std;
+class Color
+{
+private:
+    int red;
+    int blue;
+    int green;
+public:
+    int setRed(int r) {red = r; return r;}
+    int setBlue(int b) {blue = b; return b;}
+    int setGreen(int g) {green = g; return g;}
+    int getRed() {return red;}
+    int getBlue() {return blue;}
+    int getGreen() {return green;}
+    void print();
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
 }
