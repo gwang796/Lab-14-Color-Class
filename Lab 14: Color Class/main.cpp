c11a05c1c7d9fd6d318b2d628d483fe35ef08e7c
Guo An Wang <you@example.com>
2025-09-27 21:03:08 -0700

    third commit

--- Lab 14: Color Class/main.cpp
+++ Lab 14: Color Class/main.cpp
@@ -6,7 +6,11 @@
 //
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+int WIDTH = 15; //declare before class
+
 class Color
 {
 private:
@@ -15,14 +19,59 @@ private:
     int green;
 public:
     int setRed(int r) {red = r; return r;}
-    int setBlue(int b) {blue = b; return b;}
     int setGreen(int g) {green = g; return g;}
+    int setBlue(int b) {blue = b; return b;}
     int getRed() {return red;}
-    int getBlue() {return blue;}
     int getGreen() {return green;}
-    void print();
+    int getBlue() {return blue;}
+    void print(){
+        cout << setw(WIDTH) << "Red: " << red << endl;
+        cout << setw(WIDTH) << "Green: " << green << endl;
+        cout << setw(WIDTH) << "Blue: " << blue << endl;
+    }
 };
 
+void print(Color);
+
 int main(int argc, const char * argv[]) {
+    Color one;
+    one.setRed(155);
+    one.setBlue(200);
+    one.setGreen(100);
+    one.print();
+    //print(one);
     
+    Color two;
+    two.setRed(25);
+    two.setBlue(230);
+    two.setGreen(180);
+    two.print();
+    //print(two);
+
+    Color three;
+    three.setRed(255);
+    three.setBlue(10);
+    three.setGreen(80);
+    three.print();
+    //print(three);
+
+    Color four;
+    four.setRed(60);
+    four.setBlue(70);
+    four.setGreen(195);
+    four.print();
+    //print(four);
+    
+    Color five;
+    five.setRed(145);
+    five.setBlue(225);
+    five.setGreen(250);
+    five.print();
+    //print(five);
+
+
+}
+
+void print(Color c){
+    cout << c.getRed() << c.getGreen() << c.getBlue() << endl;
 }
