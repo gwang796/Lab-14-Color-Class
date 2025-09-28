579ff7ce14f7d7be464fe30645bb9fe527fdd98d
Guo An Wang <you@example.com>
2025-09-27 20:41:30 -0700

    Initial commit

---
+++ Lab 14: Color Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COMSC-210 | Lab 14: Color Class | Guo An Wang
+//  IDE used: Xcode
+//  Created by Guo An Wang on 9/27/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
